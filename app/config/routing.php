5ad47061f988897c8b63c5f2946e080d457e9307
anhnt0212 <anhnt0212@gmail.com>
2015-10-13 15:45:52 +0700

    tagcontroller

--- app/config/routing.php
+++ app/config/routing.php
@@ -4,12 +4,12 @@ return array(
     '/api/v1' => array(
     	  '/login' =>'Api\v1\AuthController:login:login-user-api::post',
         '/register' =>'Api\v1\AuthController:register:register-user-api::post',
-
-
         '/me/note' => 'Api\v1\NoteController:getPost:note_get_post',
         '/me/note/{id}' => 'Api\v1\NoteController:putDelete:note_put_delete',
         '/me/task' => 'Api\v1\TaskController:getPost:task_get_post',
         '/me/task/{id}' => 'Api\v1\TaskController:putDelete:task_put_delete',
+        '/me/tag' => 'Api\v1\TagController:Post:tag_add',
+        '/me/tag/{id}' => 'Api\v1\TagController:putDelete:pug_delete_tag',
     ),
     '/' => array(
         '/' =>'HomeController:index:home-api',
