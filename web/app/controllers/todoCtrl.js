d658851ff815610b2c8c693ecfd3e10f34b4efaa
tanUIT <12520379@gm.uit.edu.vn>
2015-10-19 17:04:53 +0700

    create service todo

--- web/app/controllers/todoCtrl.js
+++ web/app/controllers/todoCtrl.js
@@ -1,20 +1,11 @@
 'use strict';
 
 angular.module('todolist')
-	.controller('NotesController', function($scope, $http, $location) {
+	.controller('NotesController', function($scope, $http, $location, todoService) {
 	    $scope.loadNotes = function () {
-	      $http({
-	        method: 'GET',
-	        url: '/api/v1/me/note',
-	        headers: {
-	          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-	        },
-	        responseType: "json"
-	      }).then(function successCallback(response) {
-	        $scope.notes = response.data;
-	      }, function errorCallback(response) {
-	        alert('Load notes fail!');
-	      });
+			todoService.get('note').then(function(data) {
+	            $scope.notes = data;
+	        });
 	    };
 	    $scope.loadNotes();
 
@@ -24,37 +15,21 @@ angular.module('todolist')
 	    };
 
 	    $scope.addNote = function () {
-	      $http({
-	        method: 'POST',
-	        url: '/api/v1/me/note',
-	        headers: {
-	          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-	        },
-	        data: JSON.stringify({
-	          title: $scope.note
-	        }),
-	        responseType: "json"
-	      }).then(function successCallback(response) {
-	        $scope.loadNotes();
-	      }, function errorCallback(response) {
-	        alert('Add note fail!');
-	      });
+		  todoService.add('note', {title: $scope.note}).then(function (res){
+		  	$scope.loadNotes();	
+		  }, function (res) {
+		  	alert('Add note fail!');
+		  });
 	      $scope.note = "";
 	    };
 
 	    $scope.del_note = function(note_id) {    
-	      $http({
-	        method: 'DELETE',
-	        url: '/api/v1/me/note/' + note_id,
-	        headers: {
-	          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-	        },
-	        responseType: "json"
-	      }).then(function successCallback(response) {
-	        $scope.loadNotes();
-	      }, function errorCallback(response) {
-	        alert('Delete notes fail!');
-	      });
+		  todoService.delete('note/' + note_id)
+		  	.then(function (res) {
+		  		$scope.loadNotes();
+		  	}, function (res) {
+		  		alert('Delete notes fail!');
+		  	})
 	    };
 	    
 	    $scope.logout = function() {
@@ -64,18 +39,11 @@ angular.module('todolist')
 	    
 	  })
 
-	  .controller('TasksController', function($scope, $http, $routeParams, $rootScope, $location) {
+	  .controller('TasksController', function($scope, $http, $routeParams, $rootScope, $location, todoService) {
 	    $scope.loadTasks = function () { 
 	      $rootScope.addLevel = 1;
-	      $http({
-	        method: 'GET',
-	        url: '/api/v1/me/note/' + $routeParams.note_id,
-	        headers: {
-	          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-	        },
-	        responseType: "json"
-	      }).then(function successCallback(response) {
-	        $scope.tasks = response.data[0].tasks;
+	      todoService.get('note/' + $routeParams.note_id).then(function (data) {
+	      	$scope.tasks = data[0].tasks;
 	        for (var i = 0; i < $scope.tasks.length; i++) {
 	          if($scope.tasks[i].level == 1) {$scope.tasks[i].level = "info"; $scope.tasks[i].showLevel = "Low";}
 	          else if($scope.tasks[i].level == 2) {$scope.tasks[i].level = "warning"; $scope.tasks[i].showLevel = "Normal";}
@@ -85,8 +53,6 @@ angular.module('todolist')
 	          else if($scope.tasks[i].status == 2) $scope.tasks[i].status = "Pending";
 	          else $scope.tasks[i].status = "Completed";
 	        };
-	      }, function errorCallback(response) {
-	        alert('Load tasks fail!');
 	      });
 	    };
 	    $scope.loadTasks();
@@ -97,47 +63,31 @@ angular.module('todolist')
 	    };
 
 	    $scope.addTask = function () {
-	      $http({
-	        method: 'POST',
-	        url: '/api/v1/me/task',
-	        headers: {
-	          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-	        },
-	        data: JSON.stringify({
-	          content: $scope.addContent,
-	          level: parseInt($scope.addLevel),
-	          status: 1,
-	          note_id: parseInt($routeParams.note_id)
-	        }),
-	        responseType: "json"
-	      }).then(function successCallback(response) {
-	        $scope.loadTasks();
-	      }, function errorCallback(response) {
-	        alert('Add task fail!');
-	      });
+		  var data = {
+		  	content: $scope.addContent,
+			level: parseInt($scope.addLevel),
+			status: 1,
+			note_id: parseInt($routeParams.note_id)
+		  };
+
+		  todoService.add('task', data).then(function (res){
+		  	$scope.loadTasks();
+		  }, function (res) {
+		  	alert('Add task fail!');
+		  });
 	      $scope.content = "";
 	    };
 
 	    $scope.del_task = function(task_id) {    
-	      $http({
-	        method: 'DELETE',
-	        url: '/api/v1/me/task/' + task_id,
-	        headers: {
-	          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-	        },
-	        responseType: "json"
-	      }).then(function successCallback(response) {
-	        $scope.loadTasks();
-	      }, function errorCallback(response) {
-	        alert('Delete task fail!');
-	      });
+		  todoService.delete('task/' + task_id)
+		  	.then(function (res) {
+		  		$scope.loadTasks();
+		  	}, function (res) {
+		  		alert('Delete task fail!');
+		  	})
 	    };
 
-	    $scope.updated = function(key) {  
-	      // $('#content_'+key).attr('readonly', "");  
-	      // $('#status_'+key).attr('disabled', "");  
-	      // $('#update_'+key).attr('disabled', "");  
-	      // $('#edit_'+key).removeAttr('disabled');  
+	    $scope.updated = function(key) {    
 	      var level = null;
 	      if($scope.tasks[key].level == 'info') level = 1;
 	      else if($scope.tasks[key].level == 'warning') level = 2;
@@ -151,24 +101,20 @@ angular.module('todolist')
 	      else {
 	        status = parseInt($('#status_'+key).prop('value'));
 	      }
-	      $http({
-	        method: 'PUT',
-	        url: '/api/v1/me/task/' + $scope.tasks[key].id,
-	        headers: {
-	          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-	        },
-	        data: JSON.stringify({
-	          content: $('#content_'+key).prop('value'),
-	          level: level,
-	          status: status,
-	          note_id: parseInt($scope.tasks[key].note_id)
-	        }),
-	        responseType: "json"
-	      }).then(function successCallback(response) {
-	        $scope.loadTasks();
-	      }, function errorCallback(response) {
-	        alert('Update task fail!');
-	      });
+
+		  var data = {
+		  	content: $('#content_'+key).prop('value'),
+			level: level,
+			status: status,
+			note_id: parseInt($scope.tasks[key].note_id)
+		  };
+
+		  todoService.update('task/' + $scope.tasks[key].id, data)
+		  	.then(function (res) {
+		  		$scope.loadTasks();
+		  	}, function (res) {
+		  		alert('Update task fail!');
+		  	});
 	    };
 	    
 	    $scope.backUrl = function() {
@@ -184,11 +130,4 @@ angular.module('todolist')
 	    	if(event.keyCode == 13) 
 	    		$scope.updated(key);
 	    }
-
-	    // $scope.editTask = function(key) {
-	    //   $('#content_'+key).removeAttr('readonly');
-	    //   $('#status_'+key).removeAttr('disabled');
-	    //   $('#edit_'+key).attr('disabled', "");  
-	    //   $('#update_'+key).removeAttr('disabled');
-	    // };
 	});
